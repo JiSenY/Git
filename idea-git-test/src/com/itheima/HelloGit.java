fecbb8e66e9519415472e270ed2f898c997ddd97
Yjs <18713016963@163.com>
2019-06-17 15:00:01 +0800

    添加test工程至git

---
+++ idea-git-test/src/com/itheima/HelloGit.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
